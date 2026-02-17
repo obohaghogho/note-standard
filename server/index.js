428e67b625e87de4e8bfacb9e18fd0c9bfad5d80
Oboh Aghogho <obohaghogho107@gmail.com>
2026-02-17 15:52:29 +0100

    fixing message

--- server/index.js
+++ server/index.js
@@ -27,7 +27,11 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://notestandard.com", "http://localhost:5173"],
+    origin: [
+      "https://notestandard.com",
+      "https://www.notestandard.com",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -40,7 +44,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ["https://notestandard.com", "http://localhost:5173"],
+  origin: [
+    "https://notestandard.com",
+    "https://www.notestandard.com",
+    "http://localhost:5173",
+  ],
   credentials: true,
 }));
 
