fc58e21c96c8deac502ba284e7879df7d9cfcd1a
Oboh Aghogho <obohaghogho107@gmail.com>
2026-02-20 20:35:17 +0100

    chore: increase build memory and add diagnostic log

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import './i18n'
 import App from './App.tsx'
 
+console.log('🚀 NoteStandard Client Version 1.0.1 - Activity Fix Applied');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
