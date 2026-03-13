ebec8297b832c8e6262aa0095d2f0beba9a5f8be
Oboh Aghogho <obohaghogho107@gmail.com>
2026-03-13 17:45:34 +0100

    fix: prioritize websocket transport over polling for better performance

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
     credentials: true,
   },
-  transports: ["polling", "websocket"],
+  transports: ["websocket", "polling"],
   allowEIO3: true,
 });
 
