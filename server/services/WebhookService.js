63b8fe2e8ef524f4887b4001cb267d0b4dfae6b7
NoteStandard <obohaghogho107@gmail.com>
2026-07-18 18:59:27 +0100

    fix: use rawBody for Paystack webhook signature verification

--- server/services/WebhookService.js
+++ server/services/WebhookService.js
@@ -19,9 +19,7 @@ class WebhookService {
    * Verify signature of Paystack webhook
    */
   verifySignature(req) {
-    const secret = process.env.PAYSTACK_SECRET_KEY;
-    const hash = crypto.createHmac("sha512", secret).update(JSON.stringify(req.body)).digest("hex");
-    return hash === req.headers["x-paystack-signature"];
+    return WebhookSignatureService.verifyPaystack(req.headers, req.rawBody);
   }
 
   /**
